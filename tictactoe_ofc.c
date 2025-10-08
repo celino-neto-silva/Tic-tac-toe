5b018b934fb7f3e170a9072c5b1add651a5b8e19
Lucas Barbosa <dasilvanetocelino@gmai.com>
2025-10-08 11:01:04 +0100

    Table creation, array 3x3

--- tictactoe_ofc.c
+++ tictactoe_ofc.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+char table[3][3];
+
+void create_table()
+{
+    int value_ascii = 49;
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            table[i][j] = (char) value_ascii;
+            value_ascii++;
+        }
+    }
+}
+
 int main(void)
 {
     return 0;
