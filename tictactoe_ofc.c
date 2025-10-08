6ee54007a235fef35f07f34ed0eab8e97b43d71c
Lucas Barbosa <dasilvanetocelino@gmai.com>
2025-10-08 11:12:26 +0100

    Showing table function enabled

--- tictactoe_ofc.c
+++ tictactoe_ofc.c
@@ -16,7 +16,23 @@ void create_table()
     }
 }
 
+void show_table()
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            printf("|   %c  ", table[i][j]);
+        }
+        printf("|");
+        printf("\n");
+        printf("----------------------\n");
+    }
+}
+
 int main(void)
 {
+    create_table();
+    show_table();
     return 0;
 }
