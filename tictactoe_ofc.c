b59ec21175747a2aba30c4907a698538e09d946f
Lucas Barbosa <dasilvanetocelino@gmai.com>
2025-10-09 15:46:58 +0100

    Update

--- tictactoe_ofc.c
+++ tictactoe_ofc.c
@@ -33,7 +33,7 @@ void show_table()
 int validate(int position)
 {
     int check = 0;
-    char position_char = (char) position;
+    char position_char = position + '0';
 
     for(int i = 0; i < 3; i++)
     {
@@ -44,6 +44,90 @@ int validate(int position)
                 check = 1;
                 break;
             }
+        }
+    }
+    return check;
+}
+
+void insert_table(int position, char symbol)
+{
+    char position_char = position + '0';
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(table[i][j] == position_char)
+            {
+                table[i][j] = symbol;
+            }
+        }
+    }
+}
+
+char check_winner()
+{
+    char check = '\0';
+
+    while(1)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(table[i][0] == table[i][1] && table[i][1] == table[i][2])
+            {
+                check = table[i][0];
+                break;
+            }
+        }
+
+        if(check == 'X' || check == 'O')
+        {
+            break;
+        }
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(table[0][i] == table[1][i] && table[1][i] == table[2][i])
+            {
+                check = table[0][i];
+                break;
+            }
+        }
+
+        if(check == 'X' || check == 'O')
+        {
+            break;
+        }
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(table[0][0] == table[1][1] && table[1][1] == table[2][2])
+            {
+                check = table[0][0];
+                break;
+            }
+        }
+
+        if(check == 'X' || check == 'O')
+        {
+            break;
+        }
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(table[0][2] == table[1][1] && table[1][1] == table[2][0])
+            {
+                check = table[0][2];
+                break;
+            }
+        }
+
+        if(check == 'X' || check == 'O')
+        {
+            break;
+        }
+        else
+        {
+            break;
         }
     }
     return check;
@@ -64,7 +148,9 @@ void jogada(char symbol)
     {
         check = validate(position);
     }
-    while(check);
+    while(check == 0);
+
+    insert_table(position, symbol);
 }
 
 void game()
@@ -77,7 +163,24 @@ void game()
     {
         symbol = (c % 2 == 0) ? 'X' : 'O';
         show_table();
+        if(c > 4)
+        {
+            char winner = check_winner();
+
+            if(winner == 'X' || winner == 'O')
+            {
+                printf("Jogador %c! O vencedor\n", winner);
+                break;
+            }
+            else if(c == 9)
+            {
+                printf("Velha!\n");
+                break;
+            }            
+        }
         jogada(symbol);
+        system("cls");
+        
         c++;
     }
 }
