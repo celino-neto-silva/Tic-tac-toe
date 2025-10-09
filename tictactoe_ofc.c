69fc6b47dae56fa215303b2aad2fcac9132a9d08
celino-neto-silva <dasilvanetocelino@gmail.com>
2025-10-09 15:09:25 +0100

    Update tictactoe_ofc.c

--- tictactoe_ofc.c
+++ tictactoe_ofc.c
@@ -30,149 +30,54 @@ void show_table()
     }
 }
 
-char validate(char position)
+int validate(int position)
 {
-    char check = 'N';
+    int check = 0;
+    char position_char = (char) position;
 
     for(int i = 0; i < 3; i++)
     {
         for(int j = 0; j < 3; j++)
         {
-            if(position == table[i][j])
+            if(table[i][j] == position_char)
             {
-                check = 'S';
+                check = 1;
+                break;
             }
         }
     }
-
     return check;
 }
 
-void insert_table(char position, char symbol)
+void jogada(char symbol)
 {
-    for(int i = 0; i < 3; i++)
-    {
-        for(int j = 0; j < 3; j++)
-        {
-            if(table[i][j] == position)
-            {
-                table[i][j] = symbol;
-            }
-        }
-    }
-}
-
-char jogada(char symbol)
-{
-    char position;
-
+    int position;
     do
     {
         printf("Jogador [%c]: ", symbol);
-        scanf(" %c", &position);
-    }while(validate(position) == 'N');
-
-    return position;
-}
-
-char check_winner()
-{
-    char check = '\0';
-
-    while(1)
-    {
-        for(int i = 0; i < 3; i++)
-        {
-            if(table[i][0] == table[i][1] & table[i][1] == table[i][2])
-            {
-                check = table[i][0];
-                break;
-            }
-        }
-
-        if(check == 'X' || check == 'O')
-        {
-            break;
-        }
-
-        for(int i = 0; i < 3; i++)
-        {
-            if(table[0][i] == table[1][i] & table[1][i] == table[2][i])
-            {
-                check = table[0][i];
-                break;
-            }
-        }
-
-        if(check == 'X' || check == 'O')
-        {
-            break;
-        }
-
-        for(int i = 0; i < 3; i++)
-        {
-            if(table[0][0] == table[1][1] & table[1][1] == table[2][2])
-            {
-                check = table[0][0];
-                break;
-            }
-        }
-
-        if(check == 'X' || check == 'O')
-        {
-            break;
-        }
-
-        for(int i = 0; i < 3; i++)
-        {
-            if(table[0][2] == table[1][1] & table[1][1] == table[2][0])
-            {
-                check = table[0][2];
-                break;
-            }
-        }
-
-        if(check == 'X' || check == 'O')
-        {
-            break;
-        }
-        else
-        {
-            break;
-        }
+        scanf("%d", &position);
     }
-    return check;
+    while(position < 1 || position > 9);
+
+    int check = 0;
+    do
+    {
+        check = validate(position);
+    }
+    while(check);
 }
 
 void game()
 {
     create_table();
     int c = 0;
-    char position;
     char symbol;
 
     while(1)
     {
         symbol = (c % 2 == 0) ? 'X' : 'O';
         show_table();
-        position = jogada(symbol);
-        insert_table(position, symbol);
-        system("clear");
-        if(c > 4)
-        {
-            char winner = check_winner();
-
-            if(winner == 'X' || winner == 'O')
-            {
-                printf("Jogador %c! O vencedor\n", winner);
-                break;
-            }
-        }
-        else if(c == 9)
-        {
-            printf("Velha!\n");
-            break;
-        }
+        jogada(symbol);
         c++;
     }
 }
