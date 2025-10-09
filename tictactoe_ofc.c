89c69813ae6c7361ba80e2700785ed0834dedfe4
celino-neto-silva <dasilvanetocelino@gmail.com>
2025-10-09 09:10:47 +0100

    More one day! Get a choice, change player, validation and insertion is done

--- tictactoe_ofc.c
+++ tictactoe_ofc.c
@@ -30,18 +30,65 @@ void show_table()
     }
 }
 
+char validate(char position)
+{
+    char check = 'N';
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(position == table[i][j])
+            {
+                check = 'S';
+            }
+        }
+    }
+
+    return check;
+}
+
+void insert_table(char position, char symbol)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(table[i][j] == position)
+            {
+                table[i][j] = symbol;
+            }
+        }
+    }
+}
+
+char jogada(char symbol)
+{
+    char position;
+
+    do
+    {
+        printf("Jogador [%c]: ", symbol);
+        scanf(" %c", &position);
+    }while(validate(position) == 'N');
+
+    return position;
+}
+
 void game()
 {
     create_table();
     int c = 0;
+    char position;
     char symbol;
 
     while(1)
     {
         symbol = (c % 2 == 0) ? 'X' : 'O';
         show_table();
-        jogada(symbol);
-        system("cls");
+        position = jogada(symbol);
+        insert_table(position, symbol);
+        system("clear");
         c++;
         if(c == 5)
             break;
